225f42d6f0070113a0c0a13ef331238f547eff7d
Brian Pham <phambri97@gmail.com>
2022-09-30 16:54:23 -0700

    Backend setup

--- back-end/server/index.js
+++ back-end/server/index.js
@@ -1,22 +1,20 @@
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const router = require('./routes.js')
-
-import {pool} from '../db/postgres.js';
-import dotenv from 'dotenv';
-dotenv.config()
+const router = require("./routes.js");
+// const pool = require('../db/postgres.js')
 
 // pool.connect()
 
+app.use(express.json());
+app.use(cors());
 
-app.use(express.json())
-app.use(cors())
+app.use("/", router);
 
-app.use(router)
+const port = process.env.PORT || 3001;
+console.log(process.env.PORT, process.env.DB_NAME)
 
-const port = process.env.PORT || 3001
-
-app.listen((port) => {
-  console.log(`Server is running on port: ${port}`)
-})
+app.listen((port), () => {
+  console.log(`Server is running on port:${port}`);
+});
