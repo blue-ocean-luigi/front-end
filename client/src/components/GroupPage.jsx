4fc85768461e2358160caa092f746cc3ae6ccbe0
Jessie Zhao <jcz11213@gmail.com>
2022-09-30 22:12:15 -0700

    adjusted ratios of group page to account for nav sid bar, members list, group event and post feed

--- client/src/components/GroupPage.jsx
+++ client/src/components/GroupPage.jsx
@@ -11,25 +11,30 @@ import GroupMemberList from './GroupPageSubcomponents/GroupMemberList';
 
 function GroupPage() {
   return (
-    <VStack>
-      <Flex bg="tomato">
-        <VStack>
-          <Box>Search bar here</Box>
-          <Text>Plant Loverz</Text>
-          <Text>A safe space for all planty talk</Text>
-        </VStack>
-      </Flex>
-      <Flex>
-        <HStack>
-          <Box bg="blue">
-            <GroupMemberList />
-          </Box>
-          <Box bg="green">
-            <GroupFeed />
-          </Box>
-        </HStack>
-      </Flex>
-    </VStack>
+    <Flex w="100%" justifyContent="right">
+      <VStack w="90%">
+        <Flex w="100%" bg="tomato">
+          <VStack>
+            <Box>
+              <Box>Search bar here</Box>
+              <Text>Plant Loverz</Text>
+              <Text>A safe space for all planty talk</Text>
+            </Box>
+          </VStack>
+        </Flex>
+        <Flex w="100%">
+          <HStack w="100%">
+            <Box w="30%" bg="blue">
+              <GroupMemberList />
+            </Box>
+            <Box w="70%" bg="green">
+              <GroupFeed />
+            </Box>
+          </HStack>
+        </Flex>
+      </VStack>
+    </Flex>
+
 
   );
 }
