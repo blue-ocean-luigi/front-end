8c5a47db75bc2181ff7d01d55679d9c01ef13f06
Matt Waelder <mattwaelder@gmail.com>
2022-10-01 16:30:31 -0700

    added date to posts added user image to posts added search button and search input to form element added state to make http req for search added rendering for a list of friends added rendering for a list of group names made app 100% width in conjunction with rest of site

--- client/src/components/HomePage/HomePage.jsx
+++ client/src/components/HomePage/HomePage.jsx
@@ -4,7 +4,6 @@ import FriendsList from "../FriendsList.jsx";
 import HomeFeedPost from "./post/HomeFeedPost.jsx";
 import './HomePage.css';
 
-
 //  props should be user info, if user is new
 function HomePage() {
   const testobjpost = [
@@ -17,22 +16,44 @@ function HomePage() {
     { groupName: 'cat owners', description: 'we are a group of people who own cats, not just cat, but cats... lots of cats. its not an addiction, its not a problem, its okay this is fine.' },
   ];
 
+  const testfriendlist = ['apple', 'orange', 'banana', 'space shuttle'];
+
   const [newUser, setNewUser] = useState(false);
   const [posts, setPosts] = useState(testobjpost);
   const [groups, setGroups] = useState(testobjgroup);
+  const [search, setSearch] = useState('');
+
+  function handleChange(e) {
+    setSearch(e.target.value);
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log('searched for: ', search);
+    setSearch('');
+  }
 
   return (
     <div className="hp_home_feed_container">
       <div className="hp_search_bar_container">
-        <input type="text" placeholder="Search" className="hp_search_bar" />
+        <form>
+          <input type="text" placeholder="Search" className="hp_search_bar" onChange={(e) => handleChange(e)} value={search} />
+          <button type="submit" className="hp_search_btn" onClick={(e) => handleSubmit(e)}>Search!</button>
+        </form>
       </div>
       <div className="hp_body_container">
         <div className="hp_sidebar">
           <div className="hp_profile_img_container">
             <img src="https://i.pinimg.com/originals/42/90/35/429035c30c3e0aa7169168a93fdbe551.jpg" alt="alt" className="hp_profile_img" />
           </div>
-          <GroupPage />
-          <FriendsList />
+          <div className="hp_group_list_container">
+            {testobjgroup.map((group, i) => (
+              <span key= {i} className="hp_group_name"
+              onClick={()=>console.log('clicked', group.groupName)}>{group.groupName}</span>
+            )) }
+          </div>
+          <div className="hp_friends_list_container">
+              {testfriendlist.map((friend, i) => (<span key={i}className="hp_friend_name" onClick={()=>console.log('clicked: ', friend)}>{friend}</span>))}
+          </div>
         </div>
         {newUser ? (
           <div className="hp_home_feed">
@@ -58,3 +79,5 @@ export default HomePage;
 
 // should list
 // search, event feed, groups list & friends list,
+
+// need to set up so that i render a list of fiends and a list of groups in the left column, and on click those utilize the context to re render the page based on
--- client/src/components/HomePage/post/HomeFeedPost.jsx
+++ client/src/components/HomePage/post/HomeFeedPost.jsx
@@ -2,17 +2,19 @@ import React from 'react';
 import './HomeFeedPost.css';
 
 function Post({type, post}) {
-  console.log(type)
   return (
     <div className="hp_post">
       {
       type === 'post' ? (
         <>
+          <img className="hp_post_image" src="https://i.pinimg.com/originals/42/90/35/429035c30c3e0aa7169168a93fdbe551.jpg" alt="poster" />
           <p className="hp_post_name">{post.userName}</p>
           <p className="hp_post_content">{post.content}</p>
+          <span className="hp_post_date">date</span>
         </>
       ) : (
         <>
+          <img className="hp_post_image" src="https://i.pinimg.com/originals/42/90/35/429035c30c3e0aa7169168a93fdbe551.jpg" alt="poster" />
           <p className="hp_post_name">{post.groupName}</p>
           <p className="hp_post_content">{post.description}</p>
         </>
@@ -22,6 +24,3 @@ function Post({type, post}) {
   );
 }
 export default Post;
-
-// <p className="hp_post_name">{type === 'post' ? post.userName : post.groupName}</p>
-// <p className="hp_post_content">{type === 'post' ? post.content : post.description}</p>
