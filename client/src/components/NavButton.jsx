c3750b75a1ecbc2b82adce792e11c387ea406bca
James Stolhammer <jimstolhammer@gmail.com>
2022-10-01 15:20:33 -0700

    CSS Tweak on `NavButton`

--- client/src/components/NavButton.jsx
+++ client/src/components/NavButton.jsx
@@ -13,7 +13,7 @@ export default function NavButton({setMainDisplay}) {
   return (
 
     <Menu>
-      <MenuButton  position="fixed" top="0" left="0"as={Button}>Menu</MenuButton>
+      <MenuButton  variant="ghost" mt="4px" ml="4px" position="fixed" top="0" left="0"as={Button}>Menu</MenuButton>
       <MenuList>
         <MenuItem onClick={() => setMainDisplay('pages')}>Home</MenuItem>
         <MenuItem onClick={() => logout().then(() => setMainDisplay('login'))}>Log Out</MenuItem>
