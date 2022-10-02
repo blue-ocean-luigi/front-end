3b9011fba22ce2c3052ece19373f8ce60412c262
Jessie Zhao <jcz11213@gmail.com>
2022-10-01 17:30:07 -0700

    added dummy event listeners for group feed buttons, mapped dummy event data into group feed

--- client/src/components/GroupPageSubcomponents/GroupFeed.jsx
+++ client/src/components/GroupPageSubcomponents/GroupFeed.jsx
@@ -1,31 +1,54 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   Box,
-  Text,
   Button,
   Flex,
 
 } from '@chakra-ui/react';
+import FeedItem from './FeedItem';
 
 function GroupFeed() {
+  // TODO Replace hardcoded data with axios call
+  const [events, setEvents] = useState(
+    [
+      {
+        id: 0,
+        eventName: "Monstera Meetup",
+        description: "Admire people's monsteras"
+      },
+      {
+        id: 1,
+        eventName: "Monstera meetup again",
+        description: "Admire people's monsteras again"
+      },
+      {
+        id: 2,
+        eventName: "Monstera Meetup yet again",
+        description: "Admire people's monsteras yet again"
+      },
+      {
+        id: 3,
+        eventName: "Monstera Meetup",
+        description: "Admire people's monsteras"
+      },
+    ],
+  );
+
+  function onClick() {
+    console.log('clicked button');
+  }
   return (
     <Box>
       <Box position="absolute" w="100%" align="center" bg="orange">
         <Box>
-          This is where group events and posts go
-        </Box>
-        <Box>
-          This is where group events and posts go
-        </Box>
-        <Box>
-          This is where group events and posts go
+          {events.map(e => <FeedItem key={e.id} event={e} />)}
         </Box>
       </Box>
       <Flex position="absolute" bottom={0} w="100%" justifyContent="flex-end" bg="magenta">
-        <Button variant="ghost">
+        <Button variant="ghost" onClick={onClick}>
           New Event
         </Button>
-        <Button variant="ghost">
+        <Button variant="ghost" onClick={onClick}>
           New Post
         </Button>
       </Flex>
