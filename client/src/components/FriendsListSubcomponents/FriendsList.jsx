002f9580614f3b658adbabb49d44549321a77949
Matt Waelder <mattwaelder@gmail.com>
2022-10-03 12:17:11 -0700

    added friends / groups first iteration using dummy data profile page mainly done at this point

--- client/src/components/FriendsListSubcomponents/FriendsList.jsx
+++ client/src/components/FriendsListSubcomponents/FriendsList.jsx
@@ -7,7 +7,7 @@ import Friend from './Friend';
 
 function FriendsList({ friends }) {
   return (
-    <Box ml={1} align="center">
+    <Box align="center">
       {friends.map((f) => <Friend key={f.name} friend={f} />)}
     </Box>
   );
