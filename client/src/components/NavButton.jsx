37b868213b54034346acdf4728fd8998467e4cca
James Stolhammer <jimstolhammer@gmail.com>
2022-10-04 20:35:24 -0700

    Updated some logic driving login

--- client/src/components/NavButton.jsx
+++ client/src/components/NavButton.jsx
@@ -15,7 +15,7 @@ export default function NavButton({setMainPage, setPage}) {
   return (
 
     <Menu zIndex={9999}>
-      <MenuButton  variant="ghost" mt="4px" ml="4px" position="fixed" top="0" left="0"as={Button}>Menu</MenuButton>
+      <MenuButton  zIndex={9999} variant="ghost" mt="4px" ml="4px" position="fixed" top="0" left="0"as={Button}>Menu</MenuButton>
       <MenuList zIndex={9999}>
         <MenuItem zIndex={9999} onClick={() => setPage('home')}>Home</MenuItem>
         <MenuItem zIndex={9999} onClick={() => setPage('profile')}>Profile</MenuItem>
